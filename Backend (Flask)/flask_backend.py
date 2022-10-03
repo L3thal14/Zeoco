13f5677ab92257da278322faedad20a850ef5dc4
ntr0pie <42465144+ntr0pie@users.noreply.github.com>
2022-10-03 15:29:28 +0530

    Refactored code

--- Backend (Flask)/flask_backend.py
+++ Backend (Flask)/flask_backend.py
@@ -13,27 +13,27 @@ import webbrowser
 config = {
   "apiKey": "XXXX",
   "authDomain": "XXXX",
-  "databaseURL": "https://evehack-XXXX.firebaseio.com",
-  "storageBucket": "evehack-XXXXX.appspot.com",
-  "serviceAccount": "evehack.json"
+  "databaseURL": "https://zeoco-XXXX.firebaseio.com",
+  "storageBucket": "zeoco-XXXXX.appspot.com",
+  "serviceAccount": "zeoco.json"
 }
-firebase = pyrebase.initialize_app(config)#Pyrebase
-default_app = firebase_admin.initialize_app()#firebase
+firebase = pyrebase.initialize_app(config) #pyrebase
+default_app = firebase_admin.initialize_app() #firebase
 db = firebase.database()
 
-with open('/home/preet/Summerhacks/stanley.csv', 'r') as f:
-	wines = list(csv.reader(f, delimiter=','))
-wines=np.array(wines)
+with open('/home/summerhacks/zeoco/zeoco_db.csv', 'r') as f:
+	products = list(csv.reader(f, delimiter=','))
+	
+products=np.array(products)
 k = []
 data={}
 l={}
-hey = []
+users = []
 
 for user in auth.list_users().iterate_all():
-	hey.append(user.uid)
-	# a = user.uid
-	#db.child("users").update(user.uid)
-	db.child("users").child(user.uid).update({"bull":"Shit"})
+	users.append(user.uid)
+	db.child("users").child(user.uid).update({"test-key":"test-val"})
+
 def send(data,carbon_index):
 	
 	app = Flask(__name__,template_folder='template')
@@ -47,7 +47,6 @@ def send(data,carbon_index):
 
 		
 		return render_template('test.html',len=len(data),data = data)
-	print("hey")
 
 	def flaskThread():
 		 app.run()
@@ -74,29 +73,29 @@ final = []
 carb = []
 index = 0 
 while True:
-	for j in range(len(hey)):
+	for j in range(len(users)):
 		carb = []
-		print(hey[j])
-		#print(list(db.child('users').child(hey[j]).shallow().get().val())[0])
+		print(users[j])
+		#print(list(db.child('users').child(users[j]).shallow().get().val())[0])
 		
-		if(list(db.child('users').child(hey[j]).shallow().get().val())[0]=="Barcode"):
+		if(list(db.child('users').child(users[j]).shallow().get().val())[0]=="Barcode"):
 			print("Entered ")
-			barcode=str(db.child('users').child(hey[j]).child('Barcode').get().val())
+			barcode=str(db.child('users').child(users[j]).child('Barcode').get().val())
 			k=[]
 			for i in range (41):
-				if (barcode==wines[i,2]):
+				if (barcode==products[i,2]):
 					index=i
 					break
-			cat=wines[index][0]
-			less=float(wines[index][3])
+			cat=products[index][0]
+			less=float(products[index][3])
 			for i in range(41):
-				if wines[i][0]==cat:
-					carbon_index=float(wines[i][3])
+				if products[i][0]==cat:
+					carbon_index=float(products[i][3])
 					
 						  
 					if (carbon_index<less):
-						print(wines[i][1], "would be a better option :)")
-						k.append(wines[i][1])	
+						print(products[i][1], "would be a better option :)")
+						k.append(products[i][1])	
 						carb.append(carbon_index)
 
 			print(k)
@@ -104,9 +103,9 @@ while True:
 			data={"Products":k}
 			print(data)
 			final.append(data)
-			#print(hey[j])
-			db.child("users").child(hey[j]).set(data) 
-			db.child("users").child(hey[j]).update({"carbon":carb})      
+			#print(users[j])
+			db.child("users").child(users[j]).set(data) 
+			db.child("users").child(users[j]).update({"carbon":carb})      
 			send(final,carbon_index)
 
 
