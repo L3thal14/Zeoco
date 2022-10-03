890260cee87e8001d80a36b05a7233458a371ce5
ntr0pie <42465144+ntr0pie@users.noreply.github.com>
2022-10-03 15:29:52 +0530

    Renamed variables

--- Backend (Flask)/flask_backend.py
+++ Backend (Flask)/flask_backend.py
@@ -10,6 +10,8 @@ from flask import request
 from flask import render_template
 from selenium import webdriver
 import webbrowser
+
+# firebase configuration
 config = {
   "apiKey": "XXXX",
   "authDomain": "XXXX",
@@ -17,26 +19,34 @@ config = {
   "storageBucket": "zeoco-XXXXX.appspot.com",
   "serviceAccount": "zeoco.json"
 }
+
+# init firebase objects
 firebase = pyrebase.initialize_app(config) #pyrebase
 default_app = firebase_admin.initialize_app() #firebase
 db = firebase.database()
 
+# files 
 with open('/home/summerhacks/zeoco/zeoco_db.csv', 'r') as f:
 	products = list(csv.reader(f, delimiter=','))
-	
-products=np.array(products)
-k = []
-data={}
-l={}
-users = []
 
+# working variables
+products=np.array(products)
+suggested_products = []
+data={}
+users = []
+final = []
+carbon_indices = []
+index = 0 
+
+# log userbase
 for user in auth.list_users().iterate_all():
 	users.append(user.uid)
-	db.child("users").child(user.uid).update({"test-key":"test-val"})
+	db.child("users").child(user.uid).update({"test-suggested_productsey" : "test-val"})
 
+# helper functions
 def send(data,carbon_index):
 	
-	app = Flask(__name__,template_folder='template')
+	app = zeoco_suggested_products(__name__, template_folder='template')
 
 	@app.route('/', methods=['GET', 'POST'])
 	def index():
@@ -45,11 +55,11 @@ def send(data,carbon_index):
 		browser.get('http://127.0.0.1:5000/shutdown')
 		browser.close()
 
-		
 		return render_template('test.html',len=len(data),data = data)
 
 	def flaskThread():
 		 app.run()
+
 	def shutdown_server():
 		print("ok shut up")
 		func = request.environ.get('werkzeug.server.shutdown')
@@ -57,59 +67,45 @@ def send(data,carbon_index):
 			raise RuntimeError('Not running with the Werkzeug ')
 		func()
 	
-
 	@app.route('/shutdown', methods=['GET','POST'])
 	def shutdown():
 		shutdown_server()
 		return "Shutting down server"
-	
 
-		#return 'Server shutting down...'
+	#return 'Server shutting down...'
 	if __name__ == "__main__":
 			   #thread.start_new_thread(flaskThread(),)
-
 			   app.run()
-final = []
-carb = []
-index = 0 
+
 while True:
 	for j in range(len(users)):
-		carb = []
+		carbon_indices = []
 		print(users[j])
-		#print(list(db.child('users').child(users[j]).shallow().get().val())[0])
 		
 		if(list(db.child('users').child(users[j]).shallow().get().val())[0]=="Barcode"):
-			print("Entered ")
+			print("Entered user")
 			barcode=str(db.child('users').child(users[j]).child('Barcode').get().val())
-			k=[]
+			suggested_products=[]
+
 			for i in range (41):
 				if (barcode==products[i,2]):
 					index=i
 					break
-			cat=products[index][0]
+			product=products[index][0]
 			less=float(products[index][3])
+
 			for i in range(41):
-				if products[i][0]==cat:
+				if products[i][0]==product:
 					carbon_index=float(products[i][3])
-					
-						  
 					if (carbon_index<less):
 						print(products[i][1], "would be a better option :)")
-						k.append(products[i][1])	
-						carb.append(carbon_index)
+						suggested_products.append(products[i][1])	
+						carbon_indices.append(carbon_index)
 
-			print(k)
-			
-			data={"Products":k}
-			print(data)
+			# print(suggested_products)
+
+			data={"Alternative suggestions":suggested_products}
 			final.append(data)
-			#print(users[j])
 			db.child("users").child(users[j]).set(data) 
-			db.child("users").child(users[j]).update({"carbon":carb})      
+			db.child("users").child(users[j]).update({"Carbon Indices":carbon_indices})      
 			send(final,carbon_index)
-
-
-
-	
-	
-	
